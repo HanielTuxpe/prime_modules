5ab9909418a51dbd6e1756cdd5848a93b6308458
RAULCRESCENCIO <116508614+RAULCRESCENCIO@users.noreply.github.com>
2025-10-10 17:33:09 -0600

    confi para render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    allowedHosts: ["prime-modules.onrender.com"], // 👈 agrega tu dominio Render
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+  },
+});
